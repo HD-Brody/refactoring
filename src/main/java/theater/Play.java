6da1afbc0da02002ab58a898b04b7a100f903686
Brody Honigman Deltoff <0707bhd@gmail.com>
2025-11-10 11:43:54 -0500

    Complete Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,23 @@
 package theater;
 
+/**
+ * Represents a play in the theater system.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getPlayName() {
+        return name;
+    }
+
+    public String getPlayType() {
+        return type;
     }
 }
